0d2a348c52b2feefd752d6a9f64bf68793c3dd11
Fabian Andiel <fabian.andiel@gmail.com>
2024-12-13 13:02:25 +0100

    Refactoring

--- app/root.tsx
+++ app/root.tsx
@@ -12,12 +12,6 @@ import styles from "./index.css?url";
 
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
   {
     rel: "stylesheet",
     href: styles,
@@ -25,18 +19,10 @@ export const links: LinksFunction = () => [
 ];
 
 
-export async function loader() {
-  const url = 'https://api.spaceflightnewsapi.net/v4/articles';
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`);
-  }
-  const json = await response.json();
-  return json;
-}
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  const data = useLoaderData<typeof loader>();
+
   
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { css } from "styled-system/css";
+
  
 export const meta: MetaFunction = () => {
   return [
@@ -7,8 +9,20 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+
+export async function loader() {
+  const url = 'https://api.spaceflightnewsapi.net/v4/articles';
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const json = await response.json();
+  return json;
+}
  
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data)
   return (
     <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
   );

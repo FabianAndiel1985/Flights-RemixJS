3ef2d60a0247a895aaeef49c743f204c32a47fd2
Fabian Andiel <fabian.andiel@gmail.com>
2024-12-14 14:21:32 +0100

    refactors Logo Component

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,10 +1,18 @@
-import { css } from 'styled-system/css'
+import { Heading } from '@radix-ui/themes'
+import { ReactElement } from 'react'
+import { css, cx } from 'styled-system/css'
 
-const Logo = () => {
+const Logo = ():ReactElement => {
+    const flexCenter:string = css({
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center"
+    })
+
   return (
-    <div className={css({height:"100px",   display: "flex", justifyContent: "center", alignItems: "center" })}>
-        <div className={css({width:"200px",border:"2px solid black",textAlign:"center"}) } >
-            <p>Hello Im the Logo</p>
+    <div className={cx(flexCenter,css({height:"100px", mb:"40px" }))}>
+        <div className={cx(flexCenter,css({width:"400px", height:"80px",border:"2px solid black"})) } >
+            <Heading>Hello Im the Logo</Heading>
         </div>
     </div>
   )

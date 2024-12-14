a564043b35186c07611132fb69fecbb246502ebc
Fabian Andiel <fabian.andiel@gmail.com>
2024-12-14 15:15:56 +0100

    further refactoring

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,21 +1,26 @@
-import { Heading } from '@radix-ui/themes'
-import { ReactElement } from 'react'
-import { css, cx } from 'styled-system/css'
+import { Heading } from "@radix-ui/themes";
+import { ReactElement } from "react";
+import { css, cx } from "styled-system/css";
 
-const Logo = ():ReactElement => {
-    const flexCenter:string = css({
-        display: "flex", 
-        justifyContent: "center", 
-        alignItems: "center"
-    })
+const Logo = (): ReactElement => {
+  const flexCenter: string = css({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  });
 
   return (
-    <div className={cx(flexCenter,css({height:"100px", mb:"40px" }))}>
-        <div className={cx(flexCenter,css({width:"400px", height:"80px",border:"2px solid black"})) } >
-            <Heading>Hello Im the Logo</Heading>
-        </div>
+    <div className={cx(flexCenter, css({ height: "100px", mb: "40px" }))}>
+      <div
+        className={cx(
+          flexCenter,
+          css({ width: "400px", height: "80px", border: "2px solid black" })
+        )}
+      >
+        <Heading>Hello Im the Logo</Heading>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,12 +4,15 @@ import { css } from "styled-system/css";
 import ArticleOverview from "~/components/ArticleOverview";
 import Logo from "~/components/Logo";
 import { TextField } from "@radix-ui/themes";
-import { LetterCaseUppercaseIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import {
+  LetterCaseUppercaseIcon,
+  MagnifyingGlassIcon,
+} from "@radix-ui/react-icons";
 import { useState } from "react";
-import {LapTimerIcon} from "@radix-ui/react-icons";
+import { LapTimerIcon } from "@radix-ui/react-icons";
 import { sortAlphabetically, sortPublishingDate } from "~/services/sorting";
 import { ApiResponse, Article, Sorting } from "~/types/types";
- 
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Flights" },
@@ -17,75 +20,100 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader:LoaderFunction = async():Promise<ApiResponse> => {
-  const url = 'https://api.spaceflightnewsapi.net/v4/articles';
+export const loader: LoaderFunction = async (): Promise<ApiResponse> => {
+  const url = "https://api.spaceflightnewsapi.net/v4/articles";
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
   }
-  const data:ApiResponse = await response.json();
+  const data: ApiResponse = await response.json();
   return data;
-}
-
+};
 
 export default function Index() {
-  let {results} : { results: Article[] } = useLoaderData<typeof loader>();
+  let { results }: { results: Article[] } = useLoaderData<typeof loader>();
   const [query, setQuery] = useState<string>("");
-  const [tempQuery, setTempQuery] = useState<string>(""); 
-  const [wayOfSorting, setWayOfSorting]= useState<Sorting>(Sorting.Alphabetically);
+  const [tempQuery, setTempQuery] = useState<string>("");
+  const [wayOfSorting, setWayOfSorting] = useState<Sorting>(
+    Sorting.Alphabetically
+  );
 
   switch (wayOfSorting) {
-    case Sorting.PublishingDate: 
-        sortPublishingDate(results);
-    break;
-      default:
-      	sortAlphabetically(results);
+    case Sorting.PublishingDate:
+      sortPublishingDate(results);
       break;
-  }  
-
-  if(query !== "") {
-    const lowerCaseQuery = query.toLocaleLowerCase();
-    results = results.filter((elment:any)=>elment.title.toLowerCase().includes(lowerCaseQuery));
+    default:
+      sortAlphabetically(results);
+      break;
   }
 
-  return (    
+  if (query !== "") {
+    const lowerCaseQuery = query.toLocaleLowerCase();
+    results = results.filter((elment: any) =>
+      elment.title.toLowerCase().includes(lowerCaseQuery)
+    );
+  }
+
+  return (
     <>
-      <Logo/>
-    <div   
+      <Logo />
+      <div
         className={css({
-        display: "flex",
-        justifyContent: "center"
-      })}>
-        
-      <TextField.Root  placeholder="Search" className={css({width:"200px"})} 
-      value={tempQuery} onChange={(e => {setTempQuery(e.target.value)})}>
-        <TextField.Slot side={"right"}  >
-          <MagnifyingGlassIcon  height="16" width="16" onClick={(e)=>{setQuery(tempQuery)}}  className={css({_hover: { cursor: 'pointer' }})}/>
-        </TextField.Slot>
-      </TextField.Root>
-      {
-      wayOfSorting == Sorting.Alphabetically ? 
-        <LapTimerIcon  
-        className={css({_hover: { cursor: 'pointer' }, margin:"auto 0"})}
-        onClick={()=>{setWayOfSorting(Sorting.PublishingDate)}}
-        /> 
-        :
-         <LetterCaseUppercaseIcon  
-         className={css({_hover: { cursor: 'pointer' },margin:"auto 0"})}
-         onClick={()=>{setWayOfSorting(Sorting.Alphabetically)}}
-         />
-      }
-    </div>
+          display: "flex",
+          justifyContent: "center",
+        })}
+      >
+        <TextField.Root
+          placeholder="Search"
+          className={css({ width: "200px" })}
+          value={tempQuery}
+          onChange={(e) => {
+            setTempQuery(e.target.value);
+          }}
+        >
+          <TextField.Slot side={"right"}>
+            <MagnifyingGlassIcon
+              height="16"
+              width="16"
+              onClick={(e) => {
+                setQuery(tempQuery);
+              }}
+              className={css({ _hover: { cursor: "pointer" } })}
+            />
+          </TextField.Slot>
+        </TextField.Root>
+        {wayOfSorting == Sorting.Alphabetically ? (
+          <LapTimerIcon
+            className={css({ _hover: { cursor: "pointer" }, margin: "auto 0" })}
+            onClick={() => {
+              setWayOfSorting(Sorting.PublishingDate);
+            }}
+          />
+        ) : (
+          <LetterCaseUppercaseIcon
+            className={css({ _hover: { cursor: "pointer" }, margin: "auto 0" })}
+            onClick={() => {
+              setWayOfSorting(Sorting.Alphabetically);
+            }}
+          />
+        )}
+      </div>
 
       <ul>
-      { results.length > 0 &&
-        results.map((result:any)=>(
-          <li key={result.id}>
-            <ArticleOverview  imgSrc={result.image_url} title={result.title} summary={result.summary} news_site={result.news_site} published_at={result.published_at} imgLink={result.url} />
-          </li>
-        ))
-      }
-    </ul>
+        {results.length > 0 &&
+          results.map((result: any) => (
+            <li key={result.id}>
+              <ArticleOverview
+                imgSrc={result.image_url}
+                title={result.title}
+                summary={result.summary}
+                news_site={result.news_site}
+                published_at={result.published_at}
+                imgLink={result.url}
+              />
+            </li>
+          ))}
+      </ul>
     </>
   );
 }
